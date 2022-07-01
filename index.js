dd38aa096a4ada845929fce8beb8e1704a929e9c
Jianqiu Wang <jianqiuwang1@icloud.com>
2022-07-01 12:25:51 -0500

    commit tree 2a1fa5917e029648b2f53651d87cd0cb9e6fc3bf

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+
+function submitData (name,email){
+  return fetch('http://localhost:3000/users',{
+        method:'POST',
+        headers:{
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+           name,
+           email
+        })
+    })
+    .then(response=>response.json())
+    .then(obj=> document.querySelector('body').append(obj.id))
+    .catch(function(error){
+        alert('Unauthorized Access')
+        document.querySelector('body').append(error)
+
+    })
+    
+
+}
